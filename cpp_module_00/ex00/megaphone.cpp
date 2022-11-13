f49c8e150546e963272aa0f3f3316d1c63fc1996
Mou-SED <moseddik.42@gmail.com>
2022-11-13 20:40:28 +0100

    Add ex00 - megaphone

---
+++ cpp_module_00/ex00/megaphone.cpp
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: moseddik <moseddik@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/18 15:15:48 by moseddik          #+#    #+#             */
+/*   Updated: 2022/10/18 22:33:23 by moseddik         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <cstring>
+
+int main (int ac, char **av)
+{
+	int			i;
+	int			j;
+	std::string	str;
+
+	if (ac == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		exit(EXIT_FAILURE);
+	}
+	else
+	{
+		i = 1;
+		while (i < ac)
+		{
+			str = av[i];
+			j = 0;
+			while (str[j])
+			{
+				std::cout << (char)toupper(str[j]);
+				j++;
+			}
+			if (i + 1 < ac)
+				std::cout << ' ';
+			i++;
+		}
+		std::cout << std::endl;
+	}
+	return (EXIT_SUCCESS);
+}
