fbd24a517594d52c133ccb4122f49fc673a7f7b3
Mou-SED <moseddik.42@gmail.com>
2022-12-05 09:08:11 +0100

    Edit ex04 to m01 (Done)

--- cpp_module_01/ex04/main.cpp
+++ cpp_module_01/ex04/main.cpp
@@ -6,18 +6,23 @@
 /*   By: moseddik <moseddik@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/03 17:51:20 by moseddik          #+#    #+#             */
-/*   Updated: 2022/12/04 22:21:25 by moseddik         ###   ########.fr       */
+/*   Updated: 2022/12/05 09:07:23 by moseddik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Replace.hpp"
+#include <iostream>
+#include <fstream>
+
+#define RED "\033[31m"
+#define GREEN "\033[32m"
+#define RESET "\033[0m"
 
 int main(int ac, char **av)
 {
 	if (ac != 4)
 	{
 		std::cout << RED << "Error :( Invalid number of arguments" << std::endl;
-		return (1);
+		exit(EXIT_FAILURE);
 	}
 	std::string filename = av[1];
 	std::string s1 = av[2];
@@ -33,29 +38,32 @@ int main(int ac, char **av)
 	if (!fin.is_open())
 	{
 		std::cout << RED << "Error :( Can't open file" << std::endl;
-		return (1);
+		exit(EXIT_FAILURE);
 	}
-	fout.open(new_file, std::ios::out | std::ios::app);
+	fout.open(new_file, std::ios::out | std::ios::trunc);
 	if (!fout.is_open())
 	{
 		std::cout << RED << "Error :( Can't create file" << std::endl;
-		return (1);
+		(fin.close(), exit(EXIT_FAILURE));
+	}
+	if (s1 == "")
+	{
+		std::cout << RED << "Error :( s1 is empty" << std::endl;
+		(fin.close(), fout.close(), exit(EXIT_FAILURE));
 	}
 	while(getline(fin, line))
 	{
 		size_t pos = line.find(s1);
-		while (pos != std::string::npos)
+		while (s1 != s2 && pos != std::string::npos)
 		{
 			rep_line = line.substr(0, pos);
 			rep_line += s2;
 			rep_line += line.substr(pos + s1.length());
 			line = rep_line;
+			std::cout << line << std::endl;
 			pos = line.find(s1);
 		}
-		std::cout << line << std::endl;
 		fout << line << std::endl;
 	}
-	fin.close();
-	fout.close();
-	return (0);
+	return (fin.close(), fout.close(), EXIT_SUCCESS);
 }
