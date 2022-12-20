15c114c5f76db05c53d219332689ab35d237fb1a
moseddik <moussa.seddik.ph@gmail.com>
2022-12-20 15:14:40 +0100

    refactor remove out of calling funtions

--- cpp_module_02/ex03/Fixed.cpp
+++ cpp_module_02/ex03/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: moseddik <moseddik@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/11 13:02:02 by moseddik          #+#    #+#             */
-/*   Updated: 2022/12/17 12:54:34 by moseddik         ###   ########.fr       */
+/*   Updated: 2022/12/20 15:07:47 by moseddik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,27 +14,23 @@
 
 Fixed::Fixed( void ) : _fixed_point_value(0)
 {
-	std::cout << "Default constructor called" << std::endl;
 	return ;
 }
 
 Fixed::Fixed( int const value )
 {
-	std::cout << "Int constructor called" << std::endl;
 	this->_fixed_point_value = value << this->_fractional_bits;
 	return ;
 }
 
 Fixed::Fixed( float const value)
 {
-	std::cout << "Float constructor called" << std::endl;
 	this->_fixed_point_value = roundf(value * (float)(1 << this->_fractional_bits));
 	return ;
 }
 
 Fixed::Fixed( Fixed const & src )
 {
-	std::cout << "Copy constructor called" << std::endl;
 	*this = src;
 
 	return ;
@@ -42,7 +38,6 @@ Fixed::Fixed( Fixed const & src )
 
 Fixed & Fixed::operator=( Fixed const & other )
 {
-	std::cout << "Copy assignment operator called" << std::endl;
 	if (this != &other)
 		this->_fixed_point_value = other._fixed_point_value;
 
@@ -51,7 +46,6 @@ Fixed & Fixed::operator=( Fixed const & other )
 
 Fixed::~Fixed( void )
 {
-	std::cout << "Destructor called" << std::endl;
 	return ;
 }
 
