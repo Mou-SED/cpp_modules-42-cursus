7deecf0202847f79611634aefd90c745f0bdd34f
moseddik <moussa.seddik.ph@gmail.com>
2023-01-04 15:02:25 +0100

    perf: output name of Bureaucrat that not created

--- cpp_module_05/ex00/Bureaucrat.cpp
+++ cpp_module_05/ex00/Bureaucrat.cpp
@@ -6,7 +6,7 @@
 /*   By: moseddik <moseddik@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/03 15:16:56 by moseddik          #+#    #+#             */
-/*   Updated: 2023/01/04 14:34:49 by moseddik         ###   ########.fr       */
+/*   Updated: 2023/01/04 14:59:14 by moseddik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,7 +40,7 @@ Bureaucrat::Bureaucrat( std::string name, int grade ) : _name( name )
 	catch(const std::exception& e)
 	{
 		std::cerr << RED << "Exception: " << e.what() << RESET << std::endl;
-		std::cout << RED << "Your Bureaucrat has not created you must give him a Grade between 1 and 150" << RESET << std::endl;
+		std::cout << RED << "Your Bureaucrat: " << this->_name << " has not created you must give him a Grade between 1 and 150" << RESET << std::endl;
 		exit(EXIT_FAILURE);
 	}
 	return ;
