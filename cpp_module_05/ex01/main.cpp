eec9837986e4e780e8048edaf62c89b6845dfd66
moseddik <moussa.seddik.ph@gmail.com>
2023-01-04 23:30:33 +0100

    Update main for testing signForm function

--- cpp_module_05/ex01/main.cpp
+++ cpp_module_05/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: moseddik <moseddik@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/03 15:30:59 by moseddik          #+#    #+#             */
-/*   Updated: 2023/01/04 22:40:54 by moseddik         ###   ########.fr       */
+/*   Updated: 2023/01/04 23:27:40 by moseddik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,16 +20,13 @@ int main( void )
 
 	f1.beSigned( b1 );
 	std::cout << f1 << std::endl;
+	b1.signForm( f1 );
 	std::cout << std::endl;
 
 	b1.incrementGrade();
 	std::cout << GREEN << "After increment Grade of Bureaucrat-v1 by 1" << RESET << std::endl;
 	f1.beSigned( b1 );
 	std::cout << f1 << std::endl;
-
-	std::cout << std::endl;
-	std::cout << "Test copy assignment" << std::endl;
-	f2 = f1;
-	std::cout << "About f2: " << f2 << std::endl;
+	b1.signForm( f1 );
 	return 0;
 }
