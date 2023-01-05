88fafc743cd9daf2ae16f72c20490cb1374ad4da
moseddik <moussa.seddik.ph@gmail.com>
2023-01-06 00:01:58 +0100

    test: ShrubberyCreationForm class

--- cpp_module_05/ex02/main.cpp
+++ cpp_module_05/ex02/main.cpp
@@ -6,27 +6,22 @@
 /*   By: moseddik <moseddik@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/03 15:30:59 by moseddik          #+#    #+#             */
-/*   Updated: 2023/01/05 14:20:48 by moseddik         ###   ########.fr       */
+/*   Updated: 2023/01/05 20:38:03 by moseddik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "AForm.hpp"
+#include "Bureaucrat.hpp"
+#include "ShrubberyCreationForm.hpp"
 
 int main( void )
 {
-	Bureaucrat	b1( "Bureaucrat-v1", 51 );
-	AForm		f1( "AForm-v1", 50, 100 );
-	AForm		f2( "AForm-v2", 10, 120 );
+	Bureaucrat bureaucrat("Bureaucrat", 130);
 
-	f1.beSigned( b1 );
-	std::cout << f1 << std::endl;
-	b1.signForm( f1 );
-	std::cout << std::endl;
-
-	b1.incrementGrade();
-	std::cout << GREEN << "After increment Grade of Bureaucrat-v1 by 1" << RESET << std::endl;
-	f1.beSigned( b1 );
-	std::cout << f1 << std::endl;
-	b1.signForm( f1 );
+	ShrubberyCreationForm form("ShrubberyCreationForm");
+	form.beSigned(bureaucrat);
+	std::cout << form << std::endl;
+	bureaucrat.signForm(form);
+	form.execute(bureaucrat);
 	return 0;
 }
