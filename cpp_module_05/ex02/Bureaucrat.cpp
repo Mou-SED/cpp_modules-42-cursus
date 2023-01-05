b6202f77245086bcfbb1877737118ebfb51b95b3
moseddik <moussa.seddik.ph@gmail.com>
2023-01-05 14:16:37 +0100

    Add Bureaucrat class from ex01

---
+++ cpp_module_05/ex02/Bureaucrat.cpp
@@ -0,0 +1,136 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.cpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: moseddik <moseddik@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/03 15:16:56 by moseddik          #+#    #+#             */
+/*   Updated: 2023/01/04 23:47:00 by moseddik         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat( void ) : _name( "Default" ), _grade( 150 )
+{
+	return ;
+}
+
+const char *Bureaucrat::GradeTooHighException::what() const throw()
+{
+	return "Grade is Too High";
+}
+
+const char *Bureaucrat::GradeTooLowException::what() const throw()
+{
+	return "Grade is Too Low";
+}
+
+Bureaucrat::Bureaucrat( std::string name, int grade ) : _name( name )
+{
+	try
+	{
+		if (grade < 1)
+			throw GradeTooHighException();
+		if (grade > 150)
+			throw GradeTooLowException();
+		_grade = grade;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << RED << "Exception: " << e.what() << RESET << std::endl;
+		std::cout << RED << "Your Bureaucrat: " << this->_name
+			<< " has not created you must give him a Grade between 1 and 150" << RESET << std::endl;
+		Bureaucrat::~Bureaucrat();
+		e.~exception();
+		exit(EXIT_FAILURE);
+	}
+	return ;
+}
+
+Bureaucrat::Bureaucrat( Bureaucrat const & src ) : _name( src._name ), _grade( src._grade )
+{
+	return ;
+}
+
+Bureaucrat & Bureaucrat::operator=( Bureaucrat const & other )
+{
+	if (this != &other)
+	{
+		this->~Bureaucrat();
+		new (this) Bureaucrat(other);
+	}
+	return (*this);
+}
+
+std::string const Bureaucrat::getName( void ) const
+{
+	return this->_name;
+}
+
+int		Bureaucrat::getGrade( void ) const
+{
+	return this->_grade;
+}
+
+void	Bureaucrat::incrementGrade( void )
+{
+	short	newGrade = this->getGrade();
+
+	--newGrade;
+	try
+	{
+		if (newGrade < 1)
+			throw GradeTooHighException();
+		this->_grade = newGrade;
+	}
+	catch(const Bureaucrat::GradeTooHighException& e)
+	{
+		std::cerr << RED << "Exception: " << e.what() << RESET << std::endl;
+	}
+	return ;
+}
+
+void	Bureaucrat::decrementGrade( void )
+{
+	short	newGrade = this->getGrade();
+
+	++newGrade;
+	try
+	{
+		if (newGrade > 150)
+			throw GradeTooLowException();
+		this->_grade = newGrade;
+	}
+	catch(const Bureaucrat::GradeTooLowException& e)
+	{
+		std::cerr << RED << "Exception: " << e.what() << RESET << std::endl;
+	}
+	return ;
+}
+
+Bureaucrat::~Bureaucrat( void )
+{
+	return ;
+}
+
+std::ostream & operator<<( std::ostream & o, Bureaucrat const & other )
+{
+	o << other.getName() << ", bureaucrat grade " << other.getGrade();
+	return (o);
+}
+
+void	Bureaucrat::signForm( Form const & form )
+{
+	if (form.getIsSigned() == true)
+	{
+		std::cout << GREEN << this->_name << " signed " << form.getName()
+			<< RESET << std::endl;
+		return ;
+	}
+	std::cout << RED << this->_name << " couldn't sign " << form.getName()
+		<< " because " << form.getGradeToSign() << " < " << this->getGrade()
+			<< " Thus, it does not have the required Grade of Form" << std::endl;
+	return ;
+}
---
+++ cpp_module_05/ex02/Bureaucrat.hpp
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: moseddik <moseddik@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/03 14:42:25 by moseddik          #+#    #+#             */
+/*   Updated: 2023/01/04 23:06:20 by moseddik         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+
+# include <iostream>
+# include <string>
+# include <exception>
+class Form;
+# include "Form.hpp"
+
+#define GREEN "\033[0;32m"
+#define RED "\033[0;31m"
+#define RESET "\033[0m"
+
+class Bureaucrat
+{
+	private:
+		std::string const   _name;
+		int                 _grade;
+
+	public:
+		Bureaucrat( void );
+		Bureaucrat( std::string name, int grade );
+		Bureaucrat( Bureaucrat const & src );
+		~Bureaucrat( void );
+
+		Bureaucrat &operator=( Bureaucrat const & other );
+
+		std::string const   getName( void ) const;
+		int                 getGrade( void ) const;
+		void                incrementGrade( void );
+		void                decrementGrade( void );
+
+		class GradeTooHighException : public std::exception
+		{
+			public:
+				virtual const char *what() const throw();
+		};
+
+		class GradeTooLowException : public std::exception
+		{
+			public:
+				virtual const char *what() const throw();
+		};
+
+		void	signForm( Form const & form );
+};
+
+std::ostream & operator<<( std::ostream & o, Bureaucrat const & other );
+
+#endif
