f7936b708744c611c6947bd1f635a156a462b1a5
moseddik <moussa.seddik.ph@gmail.com>
2023-01-05 14:24:59 +0100

    Add main function

---
+++ cpp_module_05/ex02/main.cpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: moseddik <moseddik@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/03 15:30:59 by moseddik          #+#    #+#             */
+/*   Updated: 2023/01/05 14:20:48 by moseddik         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "AForm.hpp"
+
+int main( void )
+{
+	Bureaucrat	b1( "Bureaucrat-v1", 51 );
+	AForm		f1( "AForm-v1", 50, 100 );
+	AForm		f2( "AForm-v2", 10, 120 );
+
+	f1.beSigned( b1 );
+	std::cout << f1 << std::endl;
+	b1.signForm( f1 );
+	std::cout << std::endl;
+
+	b1.incrementGrade();
+	std::cout << GREEN << "After increment Grade of Bureaucrat-v1 by 1" << RESET << std::endl;
+	f1.beSigned( b1 );
+	std::cout << f1 << std::endl;
+	b1.signForm( f1 );
+	return 0;
+}
