997cff8b469b0144124dd74ae9834a01344f7b75
moseddik <moussa.seddik.ph@gmail.com>
2023-01-07 14:04:39 +0100

    Add tests for executeForm

--- cpp_module_05/ex02/main.cpp
+++ cpp_module_05/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: moseddik <moseddik@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/03 15:30:59 by moseddik          #+#    #+#             */
-/*   Updated: 2023/01/07 13:43:45 by moseddik         ###   ########.fr       */
+/*   Updated: 2023/01/07 14:02:48 by moseddik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,5 +39,23 @@ int main( void )
 	form.execute(bureaucrat);
 	form2.execute(bureaucrat);
 	form3.execute(bureaucrat);
+
+	std::cout << "---------------------" << std::endl;
+	Bureaucrat bureaucrat2("Bureaucrat2", 20);
+
+	RobotomyRequestForm form4("RobotomyRequestForm2");
+	PresidentialPardonForm form5("PresidentialPardonForm2");
+
+	form4.beSigned(bureaucrat2);
+	form5.beSigned(bureaucrat2);
+
+	std::cout << form4 << std::endl;
+	std::cout << form5 << std::endl;
+
+	bureaucrat2.signForm(form4);
+	bureaucrat2.signForm(form5);
+
+	bureaucrat2.executeForm(form4);
+	bureaucrat2.executeForm(form5);
 	return 0;
 }
