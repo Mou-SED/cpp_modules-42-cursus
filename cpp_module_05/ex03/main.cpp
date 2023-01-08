6b1c18c1b3d9fccf0ef909eaa0bee7d1814984a3
moseddik <moussa.seddik.ph@gmail.com>
2023-01-08 20:21:50 +0100

    perf: Update main

--- cpp_module_05/ex03/main.cpp
+++ cpp_module_05/ex03/main.cpp
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: moseddik <moseddik@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/01/03 15:30:59 by moseddik          #+#    #+#             */
-/*   Updated: 2023/01/07 23:41:43 by moseddik         ###   ########.fr       */
+/*   Created: 2023/01/08 20:17:54 by moseddik          #+#    #+#             */
+/*   Updated: 2023/01/08 20:17:58 by moseddik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,37 +20,34 @@
 int main( void )
 {
     Intern someRandomIntern;
-
     AForm* form = nullptr;
     try
     {
         form = someRandomIntern.makeForm("robotomy request", "Bender");
-        try
-        {
-            Bureaucrat b1("Bob", 5);
-            try
-            {
-                std::cout << b1 << std::endl;
-                form->beSigned(b1);
-                std::cout << *form << std::endl;
-                b1.executeForm(*form);
-            }
-            catch(const std::exception& e)
-            {
-                std::cerr << e.what() << std::endl;
-            }
-        }
-        catch(const std::exception& e)
-        {
-            std::cerr << e.what() << std::endl;
-            delete form;
-            return 1;
-        }
-        delete form;
     }
     catch(const std::exception& e)
     {
         std::cerr << RED << e.what() << RESET << std::endl;
+        return 1;
     }
-	return 0;
+    try
+    {
+        Bureaucrat b1("Bob", 5);
+        try
+        {
+            b1.signForm(*form);
+        }
+        catch(const std::exception& e)
+        {
+            std::cerr << RED << e.what() << RESET << std::endl;
+        }
+        std::cout << *form << std::endl;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << RED << e.what() << RESET << std::endl;
+        return 1;
+    }
+    delete form;
+    return 0;
 }
