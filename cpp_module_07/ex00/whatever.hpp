244296ba88db2b8337e2cc628e7233a58b6d8231
moseddik <moussa.seddik.ph@gmail.com>
2023-01-16 13:42:27 +0100

    perf: Add return by reference

--- cpp_module_07/ex00/whatever.hpp
+++ cpp_module_07/ex00/whatever.hpp
@@ -6,7 +6,7 @@
 /*   By: moseddik <moseddik@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/16 12:46:01 by moseddik          #+#    #+#             */
-/*   Updated: 2023/01/16 13:31:48 by moseddik         ###   ########.fr       */
+/*   Updated: 2023/01/16 13:41:36 by moseddik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,8 @@
 #include <string>
 #include <cstring>
 
-template<typename T>
-void	swap( T & a, T & b )
+template< typename T >
+void		swap( T & a, T & b )
 {
 	T tmp = a;
 	a = b;
@@ -27,41 +27,40 @@ void	swap( T & a, T & b )
 	return ;
 }
 
-template<typename T>
-T		min( T const & a, T const & b )
+template< typename T >
+T const &	min( T const & a, T const & b )
 {
 	return ( a < b ? a : b );
 }
 
-char *	min( char* a, char* b )
+char *		min( char* a, char* b )
 {
 	if (std::strcmp( a, b ) < 0)
 		return a;
 	return ( b );
 }
 
-const char *	min( const char* a, const char* b )
+const char*	min( const char* a, const char* b )
 {
 	if (std::strcmp( a, b ) < 0)
 		return a;
 	return ( b );
 }
 
-template<typename T>
-T		max( T const & a, T const & b )
+template< typename T >
+T const &	max( T const & a, T const & b )
 {
 	return ( a > b ? a : b );
 }
 
-
-char *	max( char* a, char* b )
+char *		max( char* a, char* b )
 {
 	if (std::strcmp( a, b ) > 0)
 		return a;
 	return ( b );
 }
 
-const char *	max( const char* a, const char* b )
+const char*	max( const char* a, const char* b )
 {
 	if (std::strcmp( a, b ) > 0)
 		return a;
