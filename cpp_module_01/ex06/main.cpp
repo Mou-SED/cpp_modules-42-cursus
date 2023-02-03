626d5fb21bb323a6d68adb1255cff7a64d97387e
Mou-SED <moseddik.42@gmail.com>
2023-02-03 18:44:34 +0100

    refactor: change out message

--- cpp_module_01/ex06/main.cpp
+++ cpp_module_01/ex06/main.cpp
@@ -6,7 +6,7 @@
 /*   By: moseddik <moseddik@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/08 15:12:40 by moseddik          #+#    #+#             */
-/*   Updated: 2022/12/08 22:58:36 by moseddik         ###   ########.fr       */
+/*   Updated: 2023/02/03 18:44:05 by moseddik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int main(int ac, char **av)
 {
 	if (ac != 2)
 	{
-		std::cout << RED << "Usage: ./a.out [DEBUG, INFO, WARNING, ERROR]" << RESET << std::endl;
+		std::cout << RED << "Usage: ./HarlFilter [DEBUG, INFO, WARNING, ERROR]" << RESET << std::endl;
 		exit(EXIT_FAILURE);
 	}
 	Harl harl;
