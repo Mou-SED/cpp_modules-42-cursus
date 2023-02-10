a26e63cdbddafa131c4838c980c119791d4e4931
Mou-SED <moseddik.42@gmail.com>
2023-02-10 17:14:33 +0100

    Fix: Don't initialize in Data struct

--- cpp_module_06/ex01/Data.hpp
+++ cpp_module_06/ex01/Data.hpp
@@ -6,7 +6,7 @@
 /*   By: moseddik <moseddik@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/15 02:14:41 by moseddik          #+#    #+#             */
-/*   Updated: 2023/01/15 02:43:43 by moseddik         ###   ########.fr       */
+/*   Updated: 2023/02/10 17:11:05 by moseddik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,9 @@
 
 struct Data
 {
-	std::string s1 = "Default";
-	int n = 0;
-	std::string s2 = "Default";
+	std::string s1;
+	int n;
+	std::string s2;
 };
 
 uintptr_t	serialize( Data* ptr );
