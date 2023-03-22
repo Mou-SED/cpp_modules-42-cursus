da130fd71aee4be2643e906ac81807c11a6bfcfe
Mou-SED <moseddik@student.1337.ma>
2023-03-22 11:54:49 +0000

    feat(C09): Init commit ex00

---
+++ cpp_module_09/ex00/BitcoinExchange.hpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   BitcoinExchange.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: moseddik <moseddik@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/22 11:49:06 by moseddik          #+#    #+#             */
+/*   Updated: 2023/03/22 11:52:57 by moseddik         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BITCOIN_EXCHANGE_HPP
+# define BITCOIN_EXCHANGE_HPP
+
+#define RED "\033[1;31m"
+#define GREEN "\033[1;32m"
+#define CYAN "\033[1;36m"
+#define RESET "\033[0m"
+
+#include <iostream>
+
+#endif
---
+++ cpp_module_09/ex00/main.cpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: moseddik <moseddik@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/22 11:48:25 by moseddik          #+#    #+#             */
+/*   Updated: 2023/03/22 11:53:14 by moseddik         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "BitcoinExchange.hpp"
+
+int main( int ac, char **av )
+{
+	(void)ac;
+	(void)av;
+	std::cout << GREEN << "BitcoinExchange is Loading...!" << RESET << std::endl;
+	return 0;
+}
